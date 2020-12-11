6745eb7b5ac0be388db02f3a369b5d3434ac5829
Frédéric Simonis <simonisfrederic@gmail.com>
2020-12-11 11:51:01 +0100

    Migrate plugin to new structure

--- _plugins/load-tutorials.rb
+++ _plugins/load-tutorials.rb
@@ -11,28 +11,20 @@ module Jekyll
         raise RuntimeError, message
       end
 
-      # Remove already registered static files of tutorials in images/tutorials
-      statics = File.join("images", "tutorials")
-      site.static_files.delete_if do |static_file|
-        relative = static_file.relative_path.delete_prefix("/")
-
-        if relative.start_with?(statics) then
-          Jekyll.logger.debug("Unregistering:", "Tutorial static #{relative}")
-          true
-        else 
-          false
-        end
-      end
+      # Contains all static files required by tutorials
+      new_static_files = []
 
       # Generate configured tutorials
       (site.config['tutorials'] || []).each do |name|
         Jekyll.logger.info("Processing:", "Tutorial #{name}")
 
+        # Common paths
         tutorial = File.join(tutorials, name)
         images = File.join(tutorial, 'images')
-        image_dest = File.join("images", "tutorials", name)
+        image_dest = File.join("images")
         readme = File.join(tutorial, 'README.md')
 
+        # Make sure the tutorial directory and README exist
         if not File.directory?(site.in_source_dir(tutorial)) then
           message ="Tutorial #{name} not found #{tutorial}"
           Jekyll.logger.error("Tutorials:", message)
@@ -45,26 +37,42 @@ module Jekyll
           raise RuntimeError, message
         end
 
-        # Register the README as a page
+        # Register the tutorial README as a page
         site.pages << Page.new(site, site.source, tutorial, 'README.md')
 
-        # Copy all images to images/tutorials/<name>/ and register as static files
+        # Copy all images to images/
         if File.directory?(site.in_source_dir(images)) then
-          FileUtils.mkdir_p(site.in_source_dir(image_dest))
           static_files = Dir.foreach(images).reject{ |f| File.directory?(f) || f.end_with?(".")}
           static_files.each do |image|
             from = File.join(images, image)
             to = File.join(image_dest, image)
             Jekyll.logger.debug("Registering:", "#{from}")
 
+            # Skip the copy if the file already exists. This solves endless rebuild loops.
             if not File.exist?(site.in_source_dir(to)) then
               Jekyll.logger.debug("Writing:", "#{to}")
               FileUtils.cp(site.in_source_dir(from), site.in_source_dir(to))
             end
-            site.static_files << StaticFile.new(site, site.source, image_dest, image)
+
+            # Save this static file for later
+            new_static_files << StaticFile.new(site, site.source, image_dest, image)
           end
         end
       end
+
+      # Prevent duplication of refisterred static files
+      site.static_files.delete_if do |static_file|
+        relative = static_file.relative_path.delete_prefix("/")
+        if new_static_files.any? { |sf| sf.relative_path == static_file.relative_path } then 
+          Jekyll.logger.debug("Removing:", "Duplicate static #{static_file.relative_path}")
+          true
+        else 
+          false
+        end
+      end
+
+      # Register all statics
+      site.static_files += new_static_files
     end
   end
 end
