ef25ff34a131518ef7d13c6a06bd445347b4852a
Frédéric Simonis <simonisfrederic@gmail.com>
2024-03-07 11:36:56 +0100

    Maybe doxygen patch fail verbose

--- doxygen/patch.sh
+++ doxygen/patch.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 
-if [[ ! -f "_config.yml" ]]; then
+if [ ! -f "_config.yml" ]; then
   >&2 echo "Call from the root of the website repo!"
   exit 1
+fi
+
+if [ ! -f "main/Doxyfile" ] || [ ! -f "develop/Doxyfile" ]; then
+   set -x
+   pwd
+   echo
+   ls -l
+   echo
+   find . -name Doxyfile
+   echo
 fi
 
 MATCH='s/^TAGFILES.*/TAGFILES = ..\/doxygen\/cppreference-doxygen-web.tag.xml=http:\/\/en.cppreference.com\/w\//'
