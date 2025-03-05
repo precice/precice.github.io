a642901dea431a35d4098e977c908892ec5f4125
fsimonis <fsimonis@users.noreply.github.com>
2025-03-05 01:47:46 +0000

    deploy: b5cd6e1e1b37526b9db6a228dd7633c62a342c76

--- doxygen/develop/search/variables_a.js
+++ doxygen/develop/search/variables_a.js
@@ -1,5 +1,6 @@
 var searchData=
 [
   ['j_0',['j',['../structprecice_1_1acceleration_1_1impl_1_1QRFactorization_1_1givensRot.html#adc95bcb455e1e7fbc22dab4ab23f43b4',1,'precice::acceleration::impl::QRFactorization::givensRot']]],
-  ['jacobiblocksize_1',['jacobiBlockSize',['../structprecice_1_1mapping_1_1MappingConfiguration_1_1GinkgoParameter.html#ad0d469f10037c8289bf60572aa32c125',1,'precice::mapping::MappingConfiguration::GinkgoParameter']]]
+  ['jacobiblocksize_1',['jacobiBlockSize',['../structprecice_1_1mapping_1_1MappingConfiguration_1_1GinkgoParameter.html#ad0d469f10037c8289bf60572aa32c125',1,'precice::mapping::MappingConfiguration::GinkgoParameter']]],
+  ['justintimemapping_2',['justInTimeMapping',['../classprecice_1_1impl_1_1DataContext.html#a3f76c6860a325c7bb442e25b1b22a8bf',1,'precice::impl::DataContext']]]
 ];
