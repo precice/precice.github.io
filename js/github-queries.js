eb2c061493ed13aa60e22ff4a93e7b82af8f8398
Your Name <raghurajramkumar927@gmail.com>
2026-03-14 21:07:31 +0530

    removed var let const added semicolon

--- js/github-queries.js
+++ js/github-queries.js
@@ -10,10 +10,10 @@
 // Get the updated field of an entry of the feed and format it like "Jan 1, 1970"
 function formatDate(updated) {
   // Extract the date and create a Date object
-  date = new Date(updated);
+  const date = new Date(updated);
 
   // Names for the months (we do not need an external library just for this)
-  var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
   // Returns e.g. "Jan 1, 1970"
   return monthNames[date.getMonth()] + ' ' + date.getDate() + ', ' + date.getFullYear();
@@ -22,16 +22,16 @@ function formatDate(updated) {
 // Wait until the complete page has loaded (so that the latest-release exists)
 document.addEventListener("DOMContentLoaded", function() {
 
-var github_api_endpoint = 'https://api.github.com/repos/precice/precice/releases'
+const github_api_endpoint = 'https://api.github.com/repos/precice/precice/releases'
 
     fetch(github_api_endpoint).then(function(response) {
       if (response.ok) {
         response.json().then(function(data) {
-          tag = data[0].name;
-          published_at = data[0].published_at;
-          url = data[0].html_url
+          const tag = data[0].name;
+          const published_at = data[0].published_at;
+          const url = data[0].html_url;
             // Format the text, which contains the link, the title, and the date.
-            var text = '<a href="' + url + '" class="btn btn-secondary no-icon action-button" role="button" target="_blank" rel="noopener noreferrer">Latest ' + tag + ' (' + formatDate(published_at) + ') &nbsp;<i class="fas fa-download"></i></a>';
+            const text = '<a href="' + url + '" class="btn btn-secondary no-icon action-button" role="button" target="_blank" rel="noopener noreferrer">Latest ' + tag + ' (' + formatDate(published_at) + ') &nbsp;<i class="fas fa-download"></i></a>';
             document.getElementById('latest-release').innerHTML = text;
         });
     }
@@ -49,13 +49,13 @@ var github_api_endpoint = 'https://api.github.com/repos/precice/precice/releases
 // Exact copy of the script to get latest release, but this time we get the number of stars, or 'stargazers_count' in Github lingo
 document.addEventListener("DOMContentLoaded", function() {
 
-    var github_api_endpoint = 'https://api.github.com/repos/precice/precice'
+    const github_api_endpoint = 'https://api.github.com/repos/precice/precice'
 
     fetch(github_api_endpoint).then(function(response) {
       if (response.ok) {
         response.json().then(function(data) {
-          count = data.stargazers_count
-          var text = '<a href="https://github.com/precice/precice/" class="btn btn-default no-icon action-button" role="button" target="_blank" rel="noopener noreferrer">Star on GitHub &nbsp;<i class="fas fa-star"></i><span id="stargazers"> ' + count + '</span></a>';
+          const count = data.stargazers_count;
+          const text = '<a href="https://github.com/precice/precice/" class="btn btn-default no-icon action-button" role="button" target="_blank" rel="noopener noreferrer">Star on GitHub &nbsp;<i class="fas fa-star"></i><span id="stargazers"> ' + count + '</span></a>';
           document.getElementById('github-button').innerHTML = text;
       });
     }
